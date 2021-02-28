fab44a867b86053a59cf507b4272ee6e6731b8da
Emin Uzun <eminzun@gmail.com>
2021-02-28 14:45:14 +0300

    project initialize

---
+++ vue-todo/src/components/header/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+    <div>Kerpeten ToDo</div>
+  </header>
+</template>
+
+<script>
+export default {
+  name:"Header"  
+}
+</script>
+
+<style scoped>
+  header{
+    width: 100%;
+    height: 50px;
+    background: #1687a7;
+    color: #f6f5f5;
+  }
+  div{
+    height: 100%;
+    line-height: 50px;
+    padding: 0px 20px;
+    font-size: 21px;
+  }
+</style>
